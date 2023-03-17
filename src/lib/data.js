b484ccbe37445f2182cbc95a2b883a5067d9ceee
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-17 16:54:29 +0100

    feat: :sparkles: add data.js with one section

---
+++ src/lib/data.js
@@ -0,0 +1,16 @@
+export const sections = {
+	A: [
+		{
+			A211: ["Länsförsäkringar", "Signup Software", "Svensk Driftpartner", "Xlent"],
+		},
+		{
+			A214: ["Vobling", "Voyado", "Warpin Media", "Swedbank"],
+		},
+		{
+			A215: ["Tutu", "Vattenfall", "MSB", "Zington", "Enfo"],
+		},
+		{
+			A218: ["Sharpness", "System Verification", "Svenska Kraftnät", "Truesec"],
+		},
+	],
+};
