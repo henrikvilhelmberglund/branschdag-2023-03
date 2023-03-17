b32933ccad4e3482606011c51f26f3fc2a0d6761
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-17 20:04:26 +0100

    feat: :sparkles: start adding company data to parse.js

---
+++ src/lib/parse.js
@@ -0,0 +1,148 @@
+export const test_data = [
+	`Accigo
+Om oss/att jobba hos oss:
+Framgång utan chefer Vi har valt att inte ha några chefer eller traditionella hierarkier, utan jobbar med något vi kallar för Kollektivt ledarskap. Det betyder att vi lägger ansvaret och möjligheterna hos oss själva. Ambitionen hos varje enskild individ är det som skapar gemensam framdrift och utveckling. När du kommer till Accigo möts du av ett kontor som bubblar av energi och professionalism. Här träffar du glada, innovativa och omtänksamma kollegor som gärna pausar en stund vid kaffemaskinen för att socialisera och bygga sitt personliga nätverk. Vi har hittat den där magiska mixen av glädje, energi och entreprenörsanda som många söker men få finner.
+
+Mer om oss:
+Se företagsfilm (https://www.youtube.com/watch?v=lqn-bVNZFAs)
+Webbsida (https://karriar.accigo.se/)
+LinkedIn (https://www.linkedin.com/company/accigo/mycompany/  )
+
+Vi är intresserade av dig som studerar:
+Business Intelligence – analytiker
+DevOps Engineer
+IT-infrastrukturspecialist
+IT-säkerhetstekniker
+Pythonutvecklare för AI
+
+Vi är intresserade av att:
+Ta emot studenter på LIA 2022
+Anställa studenter efter examen
+
+Kompetenser vi värdesätter extra/letar efter:
+Ambitionsdriven, konsultmässig, teknikintresserad
+`,
+	`Addpro
+  Om oss/att jobba hos oss:
+  På AddPro utmanar vi våra kunder inom alla områden som krävs för digital transformation; Advisory, Business Solutions & Applications, Cloud & Infrastructure och Cybersecurity. Våra managerade tjänster sträcker sig över ett mycket brett område och täcker allt från avancerad modern molnbaserad SOC, applikations- och serverdrift till högkompetent servicedesk för slutanvändare och deras enheter. Hos AddPro finns inga stora egon eller magiker. Vår superkraft är att lyssna noga, lösa kundernas 1000-bitars pussel och kavla upp ärmarna för ett seriöst dedikerat arbete. Vi ifrågasätter det förväntade och utvecklar lösningar som tar våra kunder, och oss själva, flera steg längre. Vi kallar oss the A-Team och precis som the Avengers går vi alltid all-in och stöttar våra kollegor när de behöver det som mest.
+  
+  Mer om oss:
+  Webbsida (http://www.addpro.se/)
+  LinkedIn (https://www.linkedin.com/company/addpro-ab/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BIsbeF3ZgR2OJmBQwfkEGAw%3D%3D)
+  Rörlig företagspresentation (https://youtu.be/Qs6WxfN5tvA)
+  
+  Vi är intresserade av dig som studerar:
+  Frontend-utvecklare
+  IT-infrastrukturspecialist
+  Programutvecklare .NET
+  Webbutvecklare .NET
+  Webbutvecklare fullstack open source
+  
+  Vi är intresserade av att:
+  Ta emot studenter på LIA 2023/24
+  Anställa studenter efter examen
+  
+  Kontakta mig om du har frågor:
+  sarah.stenholm@addpro.se`,
+	`Amanzia
+  Om oss/att jobba hos oss:
+  Med lång erfarenhet från konsult- och linjebefattningar, utvecklar vi våra kunders verksamhet och stöttar i förändringsarbetet. Amanzia erbjuder marknadens främsta experter som hjälper våra kunder att öka sin konkurrenskraft och nå sina affärsmål. Som företag och individer utvecklas vi i takt med vår omvärld och marknad. Vi sätter människan i centrum, är nyfikna, resultatdrivna och sätter en ära i att ha en hög leveransförmåga – alltid med omtanke och hjärta.
+  
+  Mer om oss:
+  Webbsida (http://www.amanzia.se/)
+  LinkedIn (https://www.linkedin.com/company/amanzia/)
+  
+  Vi är intresserade av dig som studerar:
+  Kvalitetssäkrare och testare inom IT
+  Programutvecklare .NET
+  Webbutvecklare .NET
+  
+  Vi är intresserade av att:
+  Ta emot studenter på LIA 2022
+  Anställa studenter efter examen
+  
+  Kontakta mig om du har frågor
+  Bifoga CV och en kort presentation av dig och berätta varför du är intresserad av att göra din praktik eller att jobba hos oss. Vi kontaktar dig ifall din profil låter intressant. Jag vill göra en intresseanmälan »
+  
+   
+  
+  Kontakta mig om du har frågor:
+  Nicklas.arnesson@amanzia.se`,
+	`Arbetsförmedlingen
+  Om oss/att jobba hos oss:
+  Välkommen till händelsernas centrum. Här arbetar du på regeringens uppdrag och på en myndighet som just nu behövs mer än någonsin. Som experter på alla typer av arbetsmarknadsfrågor bidrar vi med allt från analyser till konkreta insatser som gör att fler människor snabbare och lättare hittar jobb. Nu utvecklar vi nya arbetssätt, nya lösningar och nya sätt att samarbeta med olika aktörer i hela landet. Därför behövs fler engagerade medarbetare inom IT som vill vara med och driva förändring inom viktiga områden – är du en av dem? Vi rekryterar ständigt så håll utkik.
+  
+  Mer om oss:
+  Se företagsfilm (https://arbetsformedlingen.se/om-oss/jobba-hos-oss)
+  Webbsida (https://arbetsformedlingen.se/)
+  LinkedIn (https://www.linkedin.com/company/arbetsformedlingen/)
+  
+  Vi är intresserade av dig som studerar:
+  Business Intelligence – analytiker
+  DevOps Engineer
+  Frontend-utvecklare
+  IT-infrastrukturspecialist
+  Kvalitetssäkrare och testare inom IT
+  Programutvecklare Java
+  UX-designer
+  IT-säkerhetstekniker
+  Data Engineer
+  Webbutvecklare fullstack open source
+  Pythonutvecklare inom AI
+  
+  Kontakta mig om du har frågor
+  Du kan kontakta mig om du har några frågor:
+  mahmud.al-hakim@arbetsformedlingen.se
+  
+  Vi är intresserade av att:
+  Ta emot studenter på LIA 2022
+  Anställa studenter efter examen`,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+	``,
+];
